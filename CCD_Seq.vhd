ae2eceb9d45b587412d5f4990acdaaf0cd2a7c56
Mathias Helsen <helsen.mathias@gmail.com>
2016-05-24 17:43:19 +0200

    first state machine implemented, looks to work just fine

--- CCD_Seq.vhd
+++ CCD_Seq.vhd
@@ -11,11 +11,11 @@ entity CCD_Seq is
 	RS	: buffer std_logic;
 	BT	: buffer std_logic;
 	sample	: buffer std_logic;
-	rst	: in std_logic;
+	rst	: in std_logic
     );
-end prescaler;
+end CCD_Seq;
 
-architecture default of CDD_Seq is
+architecture default of CCD_Seq is
     type stateType is ( CLEAR, READOUT );
     signal state    : stateType := CLEAR;
     signal ctr	    : std_logic_vector(31 downto 0);	
@@ -23,11 +23,41 @@ begin
     process(clk, rst) begin
 	if( rst = '1' ) then
 	    state <= CLEAR;
-	elsif(clkIn'event and clkIn='1') then
+	elsif(clk'event and clk='1') then
 	    case state is
 		when CLEAR =>
+		    if( ctr(0) = '0' ) then
+			Ph1 <= '1';
+		    else
+			Ph1 <= '0';
+		    end if;
 
+		    if( ctr(1) = '0' ) then
+			Ph2 <= '1';
+		    else
+			Ph2 <= '0';
+		    end if;
+		    
+		    if( ctr = X"0000_0010") then
+			ctr <= X"0000_0000";
+			state <= READOUT;
+		    else
+			ctr <= ctr + X"1";
+		    end if;
 		when READOUT =>
+		    if( ctr(0) = '0' ) then
+			Ph1 <= '1';
+			Ph2 <= '0';
+		    else
+			Ph1 <= '0';
+			Ph2 <= '1';
+		    end if;
+		    if( ctr = X"0000_0020" ) then
+			ctr <= X"0000_0000";
+			state <= CLEAR;
+		    else
+			ctr <= ctr + X"1";
+		    end if;
 	    end case; 
 	end if;
     end process;
--- TestVHDL.vhd
+++ TestVHDL.vhd
@@ -14,11 +14,34 @@ architecture default of TestVHDL is
 	signal localClk : std_logic;
 	component prescaler
 		port( clkIn: in std_logic;
-				clkOut: out std_logic );
+		    clkOut: out std_logic );
+	end component;
+
+	component CCD_Seq
+		port( 
+		    clk	: in std_logic;
+		    SH	: buffer std_logic;
+		    Ph1	: buffer std_logic;
+		    Ph2	: buffer std_logic;
+		    RS	: buffer std_logic;
+		    BT	: buffer std_logic;
+		    sample	: buffer std_logic;
+		    rst	: in std_logic 
+		    );
 	end component;
 begin
-	LEDG <= outCtr;
+	-- LEDG <= outCtr;
 	U0: prescaler port map( CLOCK_50, localClk );
+
+	U1: CCD_Seq port map(
+		clk => localClk, 
+		SH => open, 
+		Ph1 => LEDG(0), 
+		Ph2 => LEDG(1), 
+		RS => open,
+		BT => open,
+		sample => open,
+		rst => '0');
 	process(localClk)
 	begin
 		if(localClk'event and localClk='1') then
