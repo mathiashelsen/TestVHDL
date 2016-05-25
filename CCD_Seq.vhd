271be7ae747cb8918e3debd15b281bb2f79c18ad
Mathias Helsen <helsen.mathias@gmail.com>
2016-05-25 10:49:47 +0200

    writing on the CCD sequencer

--- CCD_Seq.vhd
+++ CCD_Seq.vhd
@@ -20,37 +20,37 @@ architecture default of CCD_Seq is
     signal state    : stateType := CLEAR;
     signal ctr	    : std_logic_vector(31 downto 0);	
 begin
+    Ph2 <= not Ph1;
     process(clk, rst) begin
 	if( rst = '1' ) then
 	    state <= CLEAR;
 	elsif(clk'event and clk='1') then
 	    case state is
 		when CLEAR =>
-		    if( ctr(0) = '0' ) then
-			Ph1 <= '1';
-		    else
-			Ph1 <= '0';
-		    end if;
+		    case ctr is
+			when X"0000_0000" =>
+			    SH	<= '0';
+			    Ph1 <= '0';
+			when X"0000_0002" =>
+			    SH	<= '1';
+			when X"0000_002A" =>
+			    SH	<= '0';
+			when others =>
+			    null;
+		    end case;
 
-		    if( ctr(1) = '0' ) then
-			Ph2 <= '1';
-		    else
-			Ph2 <= '0';
-		    end if;
-		    
-		    if( ctr = X"0000_0010") then
+		    if( ctr = X"0000_0066") then
 			ctr <= X"0000_0000";
 			state <= READOUT;
 		    else
 			ctr <= ctr + X"1";
 		    end if;
+
 		when READOUT =>
 		    if( ctr(0) = '0' ) then
 			Ph1 <= '1';
-			Ph2 <= '0';
 		    else
 			Ph1 <= '0';
-			Ph2 <= '1';
 		    end if;
 		    if( ctr = X"0000_0020" ) then
 			ctr <= X"0000_0000";
